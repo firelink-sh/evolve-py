aa28cf47ae9bc801e5b9a6168084fbe09c7a86ab
Wilhelm Ågren <wilhelmagren98@gmail.com>
2025-08-18 23:44:27 +0200

    feat: parquet, json, csv, postgresql

--- src/evolve/source.py
+++ src/evolve/source.py
@@ -2,9 +2,15 @@ from __future__ import annotations
 
 import abc
 from pathlib import Path
-from typing import Optional
+from typing import (
+    Any,
+    Dict,
+    Optional,
+)
 
+import duckdb
 import pyarrow
+import pyarrow.parquet as pq
 from pyarrow import csv
 from pyarrow import fs
 from pyarrow import json
@@ -43,6 +49,87 @@ class Source(abc.ABC):
         pass
 
 
+class PostgresSource(Source):
+    """Implementation of a PostgreSQL source."""
+
+    def __init__(
+        self,
+        host: str,
+        db: str,
+        schema: str,
+        table: str,
+        user: str,
+        password: str,
+    ) -> None:
+        """Initialize a new PostgresSource."""
+        super(PostgresSource, self).__init__(name=self.__class__.__name__)
+
+        con = duckdb.connect()
+        con.execute("INSTALL postgres_scanner;")
+        con.execute("LOAD postgres_scanner;")
+
+        self._con = con
+        self._host = host
+        self._db = db
+        self._schema = schema
+        self._table = table
+        self._user = user
+        self._password = password
+
+    def load(self) -> pyarrow.Table:
+        """Load the PostgreSQL table to a arrow.Table."""
+        return self._con.execute(f"""  # SQL injection :)
+            SELECT * FROM postgres_scan(
+                'host={self._host} dbname={self._db}',
+                'user={self._user} password={self._password}',
+                '{self._schema}',
+                '{self._table}'
+            );
+        """).fetch_arrow_table()
+
+    def validate_config(self) -> None:
+        """Validate the PostgreSQL config."""
+        pass
+
+
+class ParquetSource(Source):
+    """Implementation of a CSV file source."""
+
+    def __init__(
+        self,
+        uri: str | Path,
+        **options: Dict[str, Any],
+    ) -> None:
+        """Initialize a new ParquetSource."""
+        super(ParquetSource, self).__init__(name=self.__class__.__name__)
+
+        if isinstance(uri, str) and "://" not in uri:
+            # Most likely a relative local path
+            uri = Path(uri).resolve()
+
+        if isinstance(uri, Path):
+            uri = "file:///" + str(uri)
+
+        file_system, path = fs.FileSystem.from_uri(uri)
+
+        self._uri = uri
+        self._file_system = file_system
+        self._path = path
+        self._options = options
+
+    def load(self) -> pyarrow.Table:
+        """Load the parquet to an Arrow table."""
+        with self._file_system.open_input_file(self._path) as f:
+            return pq.read_table(
+                source=f,
+                **self._options,
+            )
+
+    def validate_config(self) -> None:
+        """Validate the csv options."""
+        pass
+
+
 class CsvSource(Source):
     """Implementation of a CSV file source."""
 
@@ -71,6 +158,21 @@ class CsvSource(Source):
         self._path = path
         self._read_options = read_options
         self._parse_options = parse_options
+        self._convert_options = convert_options
+
+    def load(self) -> pyarrow.Table:
+        """Load the csv to an Arrow table."""
+        with self._file_system.open_input_file(self._path) as f:
+            return csv.read_csv(
+                input_file=f,
+                read_options=self._read_options,
+                parse_options=self._parse_options,
+                convert_options=self._convert_options,
+            )
+
+    def validate_config(self) -> None:
+        """Validate the csv options."""
+        pass
 
 
 class JsonSource(Source):
@@ -83,8 +185,8 @@ class JsonSource(Source):
         read_options: Optional[json.ReadOptions] = None,
         parse_options: Optional[json.ParseOptions] = None,
     ) -> None:
-        super(JsonSource, self).__init__(name=self.__class__.__name__)
         """Initialize a JsonSource."""
+        super(JsonSource, self).__init__(name=self.__class__.__name__)
         if isinstance(uri, str) and "://" not in uri:
             # Most likely a relative local path
             uri = Path(uri).resolve()
@@ -121,7 +223,12 @@ class JsonSource(Source):
           proceeds recursively on the JSON object's values.
 
         """
-        return json.read_json(self._path)
+        with self._file_system.open_input_file(self._path) as f:
+            return json.read_json(
+                input_file=f,
+                read_options=self._read_options,
+                parse_options=self._parse_options,
+            )
 
     def validate_config(self) -> None:
         """Validate the arrow json read and parse options."""
--- test.py
+++ test.py
@@ -1,9 +1,42 @@
+import time
+
 from pathlib import Path
-from src.evolve.source import JsonSource
+from src.evolve.source import (
+    CsvSource,
+    JsonSource,
+    ParquetSource,
+)
+
+import polars as pl
 
 
 if __name__ == "__main__":
-    source = JsonSource("./examples/data/dummy.json")
+    print("======================= JSON =======================")
+    json_source = JsonSource("./examples/data/dummy.json")
     # source = JsonSource(Path.cwd() / "examples" / "data" / "dummy.json")
-    df = source.load()
-    print(df)
+    json_table = json_source.load()
+    print(json_table)
+    print(pl.from_arrow(json_table))
+    time.sleep(2)
+
+    print("======================= CSV (tiny) =======================")
+    csv_source = CsvSource("examples/data/dummy.csv")
+    csv_table = csv_source.load()
+    print(csv_table)
+    print(pl.from_arrow(csv_table))
+    time.sleep(2)
+
+    print("======================= CSV (small) =======================")
+    csv_source = CsvSource(Path.cwd() / "examples" / "data" / "weather.csv")
+    csv_table = csv_source.load()
+    print(csv_table)
+    print(pl.from_arrow(csv_table))
+    time.sleep(2)
+
+    print("======================= Parquet =======================")
+    pq_source = ParquetSource(
+        "file:///home/tony/git/firelink-sh/evolve-py/examples/data/weather.parquet"
+    )
+    pq_table = pq_source.load()
+    print(pq_table)
+    print(pl.from_arrow(pq_table))
