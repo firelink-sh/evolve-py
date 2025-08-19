33849703c5f53191d634abfb6549c75efc1a923a
Wilhelm Ågren <wilhelmagren98@gmail.com>
2025-08-19 22:06:36 +0200

    feat: implement pipeline working example

--- test.py
+++ test.py
@@ -1,22 +1,69 @@
 import time
-
 from pathlib import Path
+
+import polars as pl
+import pyarrow.compute as pc
+
 from src.evolve.source import (
     CsvSource,
     JsonSource,
     ParquetSource,
 )
-
-import polars as pl
-
+from src.evolve.target import (
+    CsvTarget,
+    JsonTarget,
+    ParquetTarget,
+)
+from src.evolve.pipeline import Pipeline
+from src.evolve.transform import DropColumns
 
 if __name__ == "__main__":
+    print("READING JSON CONVERTING TO PARQUET")
+
+    pipeline = (
+        Pipeline()
+        .with_source(JsonSource("./examples/data/dummy.json"))
+        .with_target(
+            ParquetTarget(
+                "file:///home/tony/git/firelink-sh/evolve-py/xd.parquet",
+            ),
+        )
+        .with_transform(DropColumns("b"))
+    )
+
+    pipeline.run()
+
+    source = ParquetSource("xd.parquet")
+    print(source.load().get_ir())
+
+    """
     print("======================= JSON =======================")
-    json_source = JsonSource("./examples/data/dummy.json")
-    # source = JsonSource(Path.cwd() / "examples" / "data" / "dummy.json")
-    json_table = json_source.load()
+    # json_source = JsonSource("./examples/data/dummy.json")
+    json_source = JsonSource(Path.cwd() / "examples" / "data" / "dummy.json")
+    json_table = json_source.load().get_ir().to_arrow()
     print(json_table)
     print(pl.from_arrow(json_table))
+    time.sleep(2)
+
+    print("MODIFYING ARROW TABLE")
+    even_filter = pc.bit_wise_and(pc.field("b"), pc.scalar(1)) == pc.scalar(0)
+    print(pl.from_arrow(json_table))
+    json_table = json_table.filter(even_filter)
+    print(pl.from_arrow(json_table))
+    time.sleep(100)
+
+    print("Writing this JSON to csv...")
+    csv_target = CsvTarget("./xd.csv")
+    csv_target.write(json_table)
+    print("DONE!")
+    time.sleep(2)
+
+    print("Writing this JSON to parquet...")
+    pq_target = ParquetTarget(
+        "file:///home/tony/git/firelink-sh/evolve-py/xd.parquet",
+    )
+    pq_target.write(json_table)
+    print("DONE!")
     time.sleep(2)
 
     print("======================= CSV (tiny) =======================")
@@ -35,8 +82,21 @@ if __name__ == "__main__":
 
     print("======================= Parquet =======================")
     pq_source = ParquetSource(
-        "file:///home/tony/git/firelink-sh/evolve-py/examples/data/weather.parquet"
+        "file:///home/tony/git/firelink-sh/evolve-py/examples/data/weather.parquet",
     )
     pq_table = pq_source.load()
     print(pq_table)
     print(pl.from_arrow(pq_table))
+
+    print("WRITING BIG PARQUET TO JSON")
+    json_target = JsonTarget("./xd.json")
+    json_target.write(pq_table)
+    print("DONE!")
+    time.sleep(2)
+
+    print("READING THE PARQUET WE CREATED FROM THE JSON")
+    pq_source = ParquetSource("./xd.parquet")
+    pq_table = pq_source.load()
+    print(pq_table)
+    print(pl.from_arrow(pq_table))
+    """
