ed4dc74be28c808e14a53fee16e660a699b3640f
wilhelmagren <wilhelmagren98@gmail.com>
2025-09-13 22:31:21 +0200

    chore: clean up repo old test files

---
+++ examples/s3_csv_to_parquet.py
@@ -0,0 +1,14 @@
+from evole import Pipeline
+from evolve.source import CsvSource
+from evolve.target import ParquetTarget
+
+
+if __name__ == "__main__":
+    source = CsvSource("s3://bucket/path/file.csv")
+    target = ParquetTarget("s3://bucket/cleaned/file.parquet")
+    pipeline = Pipeline("example: S3 csv -> S3 parquet") \
+        .with_source(source)
+        .with_target(target)
+
+    # Pipelines are lazy by design
+    pipeline.run()
--- test-pipeline-from-yaml.py
+++
@@ -1,5 +0,0 @@
-from src.evolve.pipeline import Pipeline
-
-if __name__ == "__main__":
-    p = Pipeline.from_yaml_file("./example-pipeline.yaml")
-    print(p)
