fbef8642c1e0741c6200575cabfb1a3d23993527
Wilhelm Ågren <wilhelmagren98@gmail.com>
2025-10-06 20:15:26 +0200

    tests: arrow data write and read s3 works

--- tests/io/test_arrow_dataset.py
+++ tests/io/test_arrow_dataset.py
@@ -1,11 +1,13 @@
 import polars as pl
+import pyarrow as pa
+import pyarrow.dataset as ds
 from testcontainers.minio import MinioContainer
 
 from evolve.ir import PolarsBackend
-from evolve.io import ArrowDataset
+from evolve.io import ArrowDataset, ParquetFile
 
 
-def test_write_to_s3():
+def test_write_and_read_s3():
     with MinioContainer() as minio:
         client = minio.get_client()
         client.make_bucket("evolve-test")
@@ -18,9 +20,14 @@ def test_write_to_s3():
             {"a": [1, 2, 3], "b": [-0.4, 23.14, 9184.49], "c": [True, False, True]}
         )
 
+        print(df.head())
+
+        schema = df.drop("a").drop("b").to_arrow().schema
+        print(schema)
+
         target = ArrowDataset(
             "s3fs://evolve-test/clean/events/",
-            partitioning=["c"],
+            partitioning=ds.partitioning(schema=schema, flavor="hive"),
             access_key=minio.access_key,
             secret_key=minio.secret_key,
             endpoint_override=endpoint,
@@ -28,3 +35,31 @@ def test_write_to_s3():
         )
 
         target.write(df)
+
+        df2 = target.read()
+        assert len(df) == len(df2)
+        # partitioning column is not embedded in the parquet
+        assert len(df2.columns) == 2
+
+        print(df2.head())
+
+        files = [
+            o.object_name
+            for o in client.list_objects(
+                "evolve-test",
+                recursive=True,
+            )
+        ]
+
+        fname = [f for f in files if ".parquet" in f][0]
+
+        pp = ParquetFile(
+            "s3://evolve-test/" + fname,
+            access_key=minio.access_key,
+            secret_key=minio.secret_key,
+            endpoint_override=endpoint,
+            backend=PolarsBackend(),
+        )
+
+        dff = pp.read()
+        print(dff.head())
